e2563470db6b8635e008f3719023e2c39435ffe1
gadcad98 <37885704+gadcad98@users.noreply.github.com>
2018-09-19 23:31:08 -0700

    completed app without API call

--- src/App.js
+++ src/App.js
@@ -1,21 +1,104 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
+import pictures from "./pictures.json";
+import "./App.css";
+
+// Random shuffle
+function randompictures(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
 class App extends Component {
+  // Set this.state
+  state = {
+    pictures,
+    currentScore: 0,
+    topScore: 0,
+    correctIncorrect: "",
+    clicked: [],
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      correctIncorrect: "You guessed correctly!"
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ correctIncorrect: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      correctIncorrect: "You guessed incorrectly!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledpictures = randompictures(pictures);
+    this.setState({ pictures: shuffledpictures });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Nav
+          title="React Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          correctIncorrect={this.state.correctIncorrect}
+        />
+
+        <Title>
+          Click on an image to earn points, but don't click on any more than once!
+        </Title>
+        <Container>
+          <Row>
+            {this.state.pictures.map(friend => (
+              <Column size="md-3 sm-6">
+                <Card
+                  key={friend.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={friend.id}
+                  image={friend.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
     );
   }
 }
-
 export default App;
